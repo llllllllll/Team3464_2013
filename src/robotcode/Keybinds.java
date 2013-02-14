d4c36f0e0fd1e687412207b5c8c539bb45871393
Joe Jevnik <JoeJev@gmail.com>
2013-02-13 19:59:19 -0500

    keybinds

---
+++ src/robotcode/Keybinds.java
@@ -0,0 +1,14 @@
+
+package robotcode;
+
+/**
+ *
+ * @author JoeJev
+ */
+public class Keybinds {
+    static int toggle;
+    static int sideForward;
+    static int sideBackward;
+    static int shoulderForward;
+    static int shoulderBackward;
+}
