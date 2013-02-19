04f4734806076c2def61390abf196a92c30e266d
Jack Pugmire <jepugs@gmail.com>
2013-02-19 13:11:27 -0500

    Final operatorControl code.

--- src/robotcode/Keybinds.java
+++ src/robotcode/Keybinds.java
@@ -1,15 +1,17 @@
-
 package robotcode;
 
 /**
  *
  * @author JoeJev
  */
+
 public class Keybinds {
-    static int toggle = 10;
-    static int climbMode = 11;
-    static int sideForward = 5;
-    static int sideBackward = 4;
-    static int shoulderForward = 3;
-    static int shoulderBackward = 2;
+    static int feedDisk = 1;
+    static int start = 6;
+    static int kill = 2;
+
+    static int adjustUp = 5;
+    static int adjustDown = 3;
+
+    static int autoAim = 11;
 }
--- src/robotcode/Team3464_2013.java
+++ src/robotcode/Team3464_2013.java
@@ -19,24 +19,20 @@ public class Team3464_2013 extends SimpleRobot {
     RobotDrive drive = new RobotDrive(1,2);
     DigitalInput topSensor = new DigitalInput(1);
     DigitalInput lowSensor = new DigitalInput(2);
+
     Joystick leftJoy = new Joystick(2);
     Joystick rightJoy = new Joystick(1);
-    Jaguar winchMotor = new Jaguar(3);
-    Jaguar shoulderMotor = new Jaguar(4);
-    Jaguar leftSideMotor = new Jaguar(5);
-    Jaguar rightSideMotor = new Jaguar(6);
-    Servo frisbeeServo = new Servo(7);
-    boolean manualClimbOn = false;
+    Joystick shooterJoy = new Joystick(3);
+
+    Jaguar shooter = new Jaguar(3);
+    Victor feedme = new Victor(4);
+    DigitalInput feederFront = new DigitalInput(3);
+    DigitalInput feederBack = new DigitalInput(4);
+
     boolean dropperOpen = false;
     double joyDeadZone = 0.15;
     
-    long firstTime = -1;
-    long lastTime = -1;
-    long maxTimeDiff = 50;
-    
     long correctionTime = 50;
-    long autoWinchTime = 2000;
-    long autoArmTime = 2000;
     /**
      * The robot drives forward for forwardDriveTime, then spin clockwise until 
      * the robot finds the left side of the bottom goal. Once it finds the tape, 
@@ -44,10 +40,7 @@ public class Team3464_2013 extends SimpleRobot {
      * wall and then deliver the Frisbees to the goal.
      */
     public void autonomous() {
-        frisbeeServo.set(0);
         long startTime = System.currentTimeMillis();
-        while (System.currentTimeMillis() - startTime < forwardDriveTime)
-            drive.tankDrive(-autoSpeed, -autoSpeed);        
         while(topSensor.get() && isAutonomous()){
             drive.tankDrive(-.8*autoSpeed, .8*autoSpeed);
         }
@@ -59,74 +52,71 @@ public class Team3464_2013 extends SimpleRobot {
         while(System.currentTimeMillis() - lastTime < (lastTime-firstTime)){
             drive.tankDrive(.85*autoSpeed, -.85*autoSpeed);
         }
-        while(lowSensor.get() && isAutonomous())
-            drive.tankDrive(-autoSpeed, -autoSpeed);
-        long tmp = System.currentTimeMillis();
-        while(System.currentTimeMillis() - tmp <= correctionTime)
-            drive.tankDrive(-0.3, 0.3);
-        lastTime = System.currentTimeMillis();
-        while (System.currentTimeMillis() - lastTime < autoArmTime)
-            winchMotor.set(-.4);
-        winchMotor.set(0);
-        lastTime = System.currentTimeMillis();
-        while (System.currentTimeMillis() -lastTime < autoWinchTime)
-            winchMotor.set(-.4);
-        winchMotor.set(0);
-        frisbeeServo.set(1);
     }
 
     /**
      * 
      */
     public void operatorControl() {
-        frisbeeServo.set(0);
+	double shooterSpeed = 0.0;
+	double triggerSpeed = 0.0;
+
+	boolean top = false, bot = false, fast_deploy= false;
+	boolean feed = false;
+
         while(isOperatorControl()) {
-            while(leftJoy.getRawButton(Keybinds.climbMode))
-                manualClimbOn = true;
-            while(manualClimbOn)
-                manualClimb();
             drive.tankDrive(leftJoy.getMagnitude() < joyDeadZone ? 0 : leftJoy.getAxis(Joystick.AxisType.kY),
                     rightJoy.getMagnitude() < joyDeadZone ? 0 : rightJoy.getAxis(Joystick.AxisType.kY));
-        }
+
+	    if(!top && joy.getRawButton(adjustUp)) {
+		shooterSpeed += 0.05;
+		if(shooterSpeed > 1.0) shooterSpeed = 1.0;
+		printMsg("Shooter speed" + shooterSpeed;
+	    }
+	    if(!bot && joy.getRawButton(adjustDown)) {
+		shooterSpeed -= 0.05;
+		if(shooterSpeed < 0.0) shooterSpeed = 0.0;
+		printMsg("Shooter speed" + shooterSpeed;
+	    }
+	    if(!fast_deploy && joy.getRawButton(start)) {
+		shooterSpeed += 0.05;
+		if(shooterSpeed > 1.0) shooterSpeed = 1.0;
+		printMsg("Shooter speed" + shooterSpeed;
+	    }
+
+	    top = top != joy.getRawButton(adjustUp) ? joy.getRawButton(adjustUp) : top;
+	    bot = bot != joy.getRawButton(adjustDown) ? joy.getRawButton(adjustDown) : bot;
+	    fast_deploy = fast_deploy != joy.getRawButton(start) ? joy.getRawButton(start) : fast_deploy;
+
+	    if(joy.getRawButton(stop) && shooterSpeed != 0.0) {
+		shooterSpeed = 0.0;
+		printMsg("Shooter speed" + shooterSpeed;
+	    }
+
+	    shooter.set(shooterSpeed);
+
+	    feed = (joy.getRawButton(feedDisk) || feed) && !triggerFront.get();
+	    if(feed) {
+		triggerSpeed = 0.5;
+	    }
+	    else {
+		triggerSpeed = !triggerBack.get() ? -0.5 : 0.0;
+	    }
+
+	    feedme.set(triggerSpeed);
+	}
     }
    
+    public void printMsg(String msg) {
+
+    }
     /**
      * This function is called once each time the robot enters test mode.
      */
     public void test() {
     }
     
-    /*
-     * 
-     */
-    private void manualClimb(){
-        winchMotor.set(Math.abs(leftJoy.getMagnitude()) < joyDeadZone  ? 0 : leftJoy.getAxis(Joystick.AxisType.kY));
-        if(leftJoy.getRawButton(Keybinds.sideForward)){
-            leftSideMotor.set(.4);
-            rightSideMotor.set(.4);
-        }
-        else if(leftJoy.getRawButton(Keybinds.sideBackward)){
-            leftSideMotor.set(-.4);
-            rightSideMotor.set(-.4);
-        }
-        else{
-            leftSideMotor.set(0);
-            rightSideMotor.set(0);
-        }
-        if(leftJoy.getRawButton(Keybinds.shoulderForward))
-            shoulderMotor.set(.4);
-        else if(leftJoy.getRawButton(Keybinds.shoulderBackward))
-            shoulderMotor.set(-.4);
-        else
-            shoulderMotor.set(0);
-        if(leftJoy.getRawButton(Keybinds.toggle)){
-            frisbeeServo.set(dropperOpen ? 1. : 0.);
-            dropperOpen = false;
-        }
-        while(leftJoy.getRawButton(Keybinds.climbMode))
-            manualClimbOn = false;        
+    private void autoClimb(){
+        //>climbing
     }
-   private void autoClimb(){
-       //>climbing
-   }
 }
